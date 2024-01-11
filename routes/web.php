53cf4d42f6a97f005a832a915f329787bc84b03e
Jusin <justi2017@outlook.fr>
2024-01-11 11:02:48 +0100

    finish orm part

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,16 +21,16 @@ Route::get('/', function () {
 
 Route::prefix("/blog")->name("blog.")->group(function () {
     Route::get('/', function (Request $request) {
-        return [
-            "link" => \route('blog.show', ["slug" => "article", "id" => 13]),
-        ];
+        
+        return Post::paginate(25);
     })->name("index");
     
     Route::get("/{slug}-{id}", function (string $slug, string $id) {
-        return [
-            "slug" => $slug,
-            "id" => $id
-        ];
+        $post = Post::findOrfail($id);
+        if ($post->slug != $slug) {
+            return to_route("blog.show", [ "slug" => $post->slug, "id" => $post->id ]);
+        }
+        return $post;
     })->where([
         "id" => '[0-9]+',
         "slug" => '[a-z0-9\-]+'
